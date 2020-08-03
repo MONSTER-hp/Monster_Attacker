4424315ab3a247616f6bf53708766faa27c688c4
MONSTER_HACK <69053807+MONSTER-hp@users.noreply.github.com>
2020-08-03 16:54:59 +0430

    Delete bUgRtE.py

--- bUgRtE.py
+++
@@ -1,8 +0,0 @@
-print('`````````````````````````````````')
-print('bug rte','coding by m0bindan')
-print('DORK:inurl:RTE_POPUP_file_atch.asp')
-print('``````````````````````````````````')
-x =input('enter your sit: ')
-print('{exploit}:',x+'/rte/RTE_POPUP_file_atch.asp')
-print('or')
-print('{exploit}:',x+'/admin/RTE_POPUP_file_atch.asp')
